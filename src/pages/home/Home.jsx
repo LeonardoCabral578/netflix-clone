9f1e09daa7a8f6a7e8790e180e8acee67983f9e1
Leo <leo.cabral1016@gmail.com>
2021-12-09 03:01:55 -0300

    list1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./home.scss";
 import Navbar from "../../components/navbar/Navbar";
+import Featured from "../../components/featured/Featured";
+import List from "../../components/list/List";
 
 const Home = () => {
-    return (
-        <div className='home'>
-            <Navbar/>
-            <img width="100%" src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      <Navbar />
+      <Featured />
+      <List />
+      <List />
+      <List />
+      <List />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
