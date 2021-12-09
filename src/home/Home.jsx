5f272c18889592d3edc69d43b882e09afca9d3fc
Leo <leo.cabral1016@gmail.com>
2021-12-08 21:20:56 -0300

    navbar1

---
+++ src/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./home.scss";
+import Navbar from "../components/navbar/Navbar";
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <Navbar/>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus assumenda sed praesentium aliquam a voluptatem minima dolor, provident totam iure rem in adipisci? Adipisci sed, error culpa recusandae aspernatur expedita.
+        </div>
+    )
+}
+
+export default Home
