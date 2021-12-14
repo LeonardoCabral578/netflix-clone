23a7807f81a255db4b3169ea05d9bd8d8c9f144a
Leo <leo.cabral1016@gmail.com>
2021-12-14 17:06:20 -0300

    auth

--- api/index.js
+++ api/index.js
@@ -2,14 +2,22 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(()=>console.log("DB Connection Successfull!")).catch((err) => console.log(err));
+  })
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => console.log(err));
 
-app.listen(8800, ()=>{
-    console.log("Backend server is running!")
-})
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
